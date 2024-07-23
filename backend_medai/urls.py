32fc19057c44cd51fac50d4320585551714881fd
juandtap <jtapiav4@est.ups.edu.ec>
2024-07-23 02:34:51 -0500

    cambios urls

--- backend_medai/urls.py
+++ backend_medai/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/subirtomografia', include('brain_tumor_segmentation.urls')),
+    path('api/', include('brain_tumor_segmentation.urls')),
 
 ]
