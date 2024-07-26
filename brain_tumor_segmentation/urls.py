6a10e5e7bd0a422bf3bb916ae48ee3d4ebacc2d8
Andres Alvarado <nalvaradoc1@est.ups.edu.ec>
2024-07-25 23:16:10 -0500

    implementacion de la api de hugging-face

--- brain_tumor_segmentation/urls.py
+++ brain_tumor_segmentation/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from .views import cargar_imagen
+from .views import subir_audio
 
 urlpatterns = [
     path('subirtomografia', cargar_imagen, name='cargar_imagen'),
+    path('subiraudio', subir_audio, name='subir_audio')
 ]
