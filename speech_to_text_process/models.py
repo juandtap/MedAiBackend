ddeaed96f158a2d91ba23acc67d3d99d05af1f09
Paul-Astudillo <129395539+Paul-Astudillo@users.noreply.github.com>
2024-07-28 23:41:29 -0500

    modelo de Base de datos

--- speech_to_text_process/models.py
+++ speech_to_text_process/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=100)
+    id_paciente = models.CharField(max_length=50, unique=True)  # Usamos 'id_paciente' para evitar conflicto con el campo 'id' por defecto
+    edad = models.IntegerField()
+    genero = models.CharField(max_length=10)
+    direccion = models.CharField(max_length=255)
+    celular = models.CharField(max_length=15)
+    correo = models.EmailField()
+    diagnostico = models.TextField()
+    alergias = models.TextField()
+
+    def __str__(self):
+        return self.nombre
