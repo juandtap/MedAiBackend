28c391b3a946cabcf001f2087ab47faca8f97409
juandtap <jtapiav4@est.ups.edu.ec>
2024-07-30 12:39:54 -0500

    prediccion enviada al front

--- brain_tumor_segmentation/ml_model/segmentation.py
+++ brain_tumor_segmentation/ml_model/segmentation.py
@@ -23,6 +23,7 @@ import numpy as np
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.patches as mpatches
 
 from skimage import measure
 
@@ -97,13 +98,62 @@ class ModeloSegmentacion:
         norm = mpl.colors.BoundaryNorm([-0.5, 0.5, 1.5, 2.5, 3.5], cmap.N)
 
         axstest.imshow(my_pred, cmap, norm)
-        axstest.set_title('Predición')
+        #axstest.set_title('Predición')
 
         # Ocultar los ejes
         axstest.axis('off')
+
+        # Crear la leyenda
+        legend_labels = ['núcleo tumoral necrótico (NCR)', 'tejido edematoso peritumoral (ED)', 'tumor realresaltado (ET)']
+        legend_colors = ['#3b528b', '#18b880', '#e6d74f']
+        patches = [mpatches.Patch(color=color, label=label) for color, label in zip(legend_colors, legend_labels)]
+
+        axstest.legend(handles=patches, loc='upper right', bbox_to_anchor=(1.1, 1))
 
         plt.subplots_adjust(wspace=0.8)
         output_path = f'{sample_path}_prediction.png'
         plt.savefig(output_path)
         plt.close()
+        return output_path
+
+    def obtener_original(self, nii_path, corte):
+        # Cargar la imagen .nii.gz
+        img = nib.load(nii_path)
+        data = img.get_fdata()
+
+        # Extraer el corte axial específico
+        corte_axial = data[:, :, corte]
+
+        # Voltear la imagen verticalmente
+        corte_axial_flipped = np.flipud(corte_axial)
+
+        # Crear una figura y un eje sin mostrar los ejes
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.imshow(corte_axial_flipped, cmap='gray', origin='lower')
+        ax.axis('off')  # Ocultar los ejes
+
+        # Guardar la imagen como PNG
+        output_path = f'{nii_path}_corte_{corte}.png'
+        plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+        return output_path
+
+    def combinar_imagenes(self, img_path1, img_path2, alpha=0.5, beta=0.5, gamma=0):
+        # Cargar las dos imágenes
+        img1 = cv2.imread(img_path1, cv2.IMREAD_UNCHANGED)
+        img2 = cv2.imread(img_path2, cv2.IMREAD_UNCHANGED)
+        print("path de la imagen 1", img_path1)
+        # Redimensionar la segunda imagen para que tenga el mismo tamaño que la primera
+        img2_resized = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
+
+        # Combinar las imágenes con pesos específicos
+        result = cv2.addWeighted(img1, alpha, img2_resized, beta, gamma)
+
+        # Guardar la imagen combinada
+        abspath = img_path2
+        abspath = abspath.replace('tomografia_prediction.png', '')
+        output_path = abspath+'imagen_combinada.png'
+        cv2.imwrite(output_path, result)
+
         return output_path
